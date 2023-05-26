e877942fe84149de5dc4d14f60142ba55e3f49db
sanjeeb tiwary <63203112+sanjeebtiwary@users.noreply.github.com>
2023-05-26 12:56:27 +0530

    load_predict.py and model.ipnb Update

--- load_predict.py
+++ load_predict.py
@@ -1,28 +1,66 @@
 import os
-import torch
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
-import pickle
+import cv2
+import numpy as np
+from sklearn.mixture import GaussianMixture
+from tensorflow import keras
 
-# Load the trained model
-iris_model = nn.Sequential(
-            nn.Linear(4,32),
-            nn.Tanh(),
-            nn.Linear(32,3),
-            nn.LogSoftmax(dim=1))
+def extract_features(frames):
+    features = []
 
-# iris_model.load_state_dict(torch.load('/kaggle/input/iris-pymodel/iris_model.pt'))
+    # Calculate optical flow for consecutive frames
+    for i in range(len(frames) - 1):
+        frame1 = frames[i]
+        frame2 = frames[i + 1]
 
-iris_model.load_state_dict(torch.load("C:\\Users\\HOME\\Zummit_2023\\model_template.pt"))
+        # Calculate optical flow
+        flow = cv2.calcOpticalFlowFarneback(frame1, frame2, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
-iris_model.eval() # переводим модель в режим применения
-input_tensor = torch.tensor([[4.9,2.4,3.3,1.0]])
+        # Flatten the flow matrix and append it to the features
+        flow_flat = flow.reshape(-1)
+        features.append(flow_flat)
 
-# Predictions
-outputs = iris_model(input_tensor)
-outputs = iris_model(input_tensor)
+    return features
 
-_, predicted = torch.max(outputs, dim=1)
 
-print('Output Tensor:\n', outputs)
-print('\nTorch Max Function Result: ', predicted)
+def main():
+    # Load the saved MDT model
+    mdt_model = keras.models.load_model('model.h5')
+
+    # Specify the directory path containing the frames to predict
+    frames_directory = 'D:/Data Set/Crowd Anomaly Detection/UCSD_Anomaly_Dataset/UCSDped1/Test/Test001'
+
+    # Initialize an empty list to store the frames
+    frames = []
+
+    # Iterate over the files in the directory
+    for filename in os.listdir(frames_directory):
+        file_path = os.path.join(frames_directory, filename)
+
+        # Check if the file is an image
+        if filename.endswith('.jpg') or filename.endswith('.png'):
+            # Read the frame using OpenCV
+            frame = cv2.imread(file_path)
+
+            # Preprocess the frame if needed
+
+            # Append the frame to the list
+            frames.append(frame)
+
+    # Convert frames to numpy array
+    frames = np.array(frames)
+
+    # Extract features from the frames
+    features = extract_features(frames)
+
+    # Convert features to numpy array
+    features = np.array(features)
+
+    # Reshape features for prediction
+    num_samples, num_frames, num_features = features.shape
+    features_reshaped = features.reshape(num_samples * num_frames, num_features)
+
+    # Predict using the MDT model
+    predictions = mdt_model.predict(features_reshaped)
+
+if __name__ == '__main__':
+    main()
